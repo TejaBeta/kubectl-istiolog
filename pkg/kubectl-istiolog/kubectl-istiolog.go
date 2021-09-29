8b197187ea324c79cc7e6e6edbeaa1737526bd23
Tejasvi Thota <tejasvi.thota@gmail.com>
2021-09-29 18:42:54 +0100

    Add basic pod checker

--- pkg/kubectl-istiolog/kubectl-istiolog.go
+++ pkg/kubectl-istiolog/kubectl-istiolog.go
@@ -15,8 +15,42 @@ limitations under the License.
 package pkg
 
 import (
+	"context"
+
 	log "github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
+
+type options struct {
+	clientset kubernetes.Interface
+	namespace string
+}
+
+func getOpts(context *rest.Config, ns string) (*options, error) {
+	cs, err := kubernetes.NewForConfig(context)
+	if err != nil {
+		return nil, err
+	}
+
+	return &options{
+		clientset: cs,
+		namespace: ns,
+	}, nil
+}
+
+func (kOpts *options) isPodExists(podName string) bool {
+	result, err := kOpts.clientset.CoreV1().Pods(kOpts.namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		for _, pod := range result.Items {
+			if pod.Name == podName {
+				return true
+			}
+		}
+	}
+	return false
+}
 
 func KubectlIstioLog(pod string, namespace string, logLevel string, follow bool) {
 	log.Println(pod, namespace, logLevel, follow)
